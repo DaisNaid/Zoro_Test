b335bf35bc02b9b28e49f54f60ed58833c09ea4d
Tryve <iamtryve@gmail.com>
2022-09-04 10:34:46 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  HashRouter,
+  NavLink,
+} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import search_icon from './css-test-2022/icons/search-outline.svg';
+import menu from './css-test-2022/icons/menu-outline.svg';
+import close from './css-test-2022/icons/close.svg';
+import HomeScreen from './screens/HomeScreen';
+import Facebook from './css-test-2022/icons/facebook-outline.svg';
+import Twitter from './css-test-2022/icons/twitter-outline.svg';
 
 function App() {
+  //Toggles menu
+  const toggle_menu = () => {
+    const the_menu = document.querySelector('.menu');
+    the_menu.classList.toggle('is-open');
+  };
+  //Closes menu after a selection is made
+  const revert = () => {
+    const the_menu = document.querySelector('.menu');
+    the_menu.classList.toggle('is-open');
+  };
+
+  const toggle_search = () => {
+    const the_search = document.querySelector('.search-space');
+    the_search.classList.toggle('open');
+  };
+
+  const revert_search = () => {
+    const close_search = document.querySelector('.search-space');
+    close_search.classList.toggle('open');
+  };
+
+  //Built to run as a single page app
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <div>
+          <ToastContainer position="bottom-center" limit={1} />
+          <header>
+            <h2>ART GALLERY</h2>
+            <img onClick={toggle_search} src={search_icon} alt="Search" />
+            <div className="search-space">
+              <img
+                className="search-close"
+                onClick={revert_search}
+                src={close}
+                alt="menu"
+              />
+              <input type="text" name="search" placeholder="Search.." />
+            </div>
+            <div className="hamburger" onClick={toggle_menu}>
+              <img src={menu} alt="menu" />
+            </div>
+            <div className="menu">
+              <ul className="menu-list" style={{ listStyle: 'none' }}>
+                <div className="hamburger close" onClick={toggle_menu}>
+                  <img src={close} alt="menu" />
+                </div>
+                <li onClick={revert}>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li onClick={revert}>
+                  <NavLink to="/art">Art & Artists</NavLink>
+                </li>
+                <li onClick={revert}>
+                  <NavLink to="/exhibitions">Exhibitions</NavLink>
+                </li>
+                <li onClick={revert}>
+                  <NavLink to="/news">News</NavLink>
+                </li>
+                <li onClick={revert}>
+                  <NavLink to="/about">About</NavLink>
+                </li>
+                <li onClick={revert}>
+                  <NavLink to="/p-area">Private area</NavLink>
+                </li>
+              </ul>
+            </div>
+          </header>
+          <main>
+            <Container className="site-container">
+              <Routes>
+                <Route path="/" element={<HomeScreen />} />
+                <Route path="art" />
+                <Route path="exhibitions" />
+                <Route path="news" />
+                <Route path="about" />
+                <Route path="p-area" />
+              </Routes>
+            </Container>
+          </main>
+          <footer>
+            <div className="footer-head">
+              <ul className="font-face-bold title footer-list">
+                <li>
+                  <NavLink to="/newsletter">Newsletter</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/p-room">Press room</NavLink>
+                </li>
+              </ul>
+              <img src={Facebook} alt="Facebook" />
+              <img src={Twitter} alt="Twitter" />
+            </div>
+            <div className="address">
+              <span>ART GALLERY</span>
+              <span>38-40 Southwark Street</span>
+              <span>London SE1 1UN</span>
+              <a href="tel: +442074905337">
+                <span>Telephone +44 20 74905337</span>
+              </a>
+            </div>
+            <div>&copy; 2022 EC1 GALLERY</div>
+          </footer>
+        </div>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,200 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Banner from '../css-test-2022/images/banner.jpg';
+import news1 from '../css-test-2022/images/news-1.jpg';
+import news2 from '../css-test-2022/images/news-2.jpg';
+import news3 from '../css-test-2022/images/news-3.jpg';
+import arrow from '../css-test-2022/icons/arrow-forward-outline.svg';
+import Youtube from '../css-test-2022/images/youtube.jpg';
+import Artist from '../css-test-2022/images/artist.jpg';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { Link } from 'react-router-dom';
+import { data } from '../artistList';
+
+export default function HomeScreen() {
+  const displayImage = () => {
+    const image = document.querySelector('.artist-image');
+    image.classList.toggle('is-Called');
+  };
+  const hideImage = () => {
+    const image = document.querySelector('.artist-image');
+    image.classList.toggle('is-Called');
+  };
+
+  return (
+    <Container className="site-container">
+      <article className="banner article-main">
+        <AliceCarousel
+          autoPlay="true"
+          autoPlayStrategy="none"
+          autoPlayInterval="2500"
+          infinite="true"
+          disableButtonsControls="true"
+        >
+          <div className="sliderContainer">
+            <img className="sliderImg" src={Banner} alt="Banner" />
+            <span className="font-face-bold title">
+              <Link className="banner-link" to="#">
+                Anselm Kiefer: Walhalla
+              </Link>
+            </span>
+            <span>Until 12 February 2017, London</span>
+          </div>
+          <div className="sliderContainer">
+            <img className="sliderImg" src={Banner} alt="Banner" />
+            <span className="font-face-bold title">
+              <Link className="banner-link" to="#">
+                Anselm Kiefer: Walhalla //second slide
+              </Link>
+            </span>
+            <span>Until 12 February 2017, London</span>
+          </div>
+          <div className="sliderContainer">
+            <img className="sliderImg" src={Banner} alt="Banner" />
+            <span className="font-face-bold title">
+              <Link className="banner-link" to="#">
+                Anselm Kiefer: Walhalla //third slide
+              </Link>
+            </span>
+            <span>Until 12 February 2017, London</span>
+          </div>
+        </AliceCarousel>
+      </article>
+      <article className="n-e">
+        <div className="article-head">
+          <span className="font-face-bold title">News & Events</span>
+          <span>
+            <Link className="banner-link" to="#">
+              View All
+            </Link>
+          </span>
+          <img src={arrow} alt="arrow" />
+        </div>
+        <div className="article-main">
+          <div className="news1">
+            <div className="combined-hover">
+              <img src={news1} alt="news1" />
+              <span className="font-face-bold title">
+                Exhibition: He Xiangyu monograph released by Distanz
+              </span>
+            </div>
+            <span>From March 2016</span>
+            <span>
+              He Xiangyu and Liu Wei are among 74 international artists from 33
+              countries chosen to feature in the Yinchuan Biennale 2016
+            </span>
+          </div>
+          <div className="news2">
+            <div className="combined-hover">
+              <img src={news2} alt="news2" />
+              <span className="font-face-bold title">
+                Exhibition: Runa Islam at SFMOMA, San Francisco
+              </span>
+            </div>
+            <span>From March 2016</span>
+            <span>
+              'Verso', SFMOMA's solo presentation of Bangladeshi-born British
+              artist Runa Islam, features the US premiere of Cabinet of
+              Prototypes (2009-10)
+            </span>
+          </div>
+          <div className="news3">
+            <a href="#">
+              <div className="combined-hover">
+                <img src={news3} alt="news3" />
+                <span className="font-face-bold title">
+                  Preview: White Cube Mason's Yard
+                </span>
+              </div>
+            </a>
+            <span>19 January 2017, 6 to 8pm</span>
+            <span>
+              Join us at Mason's Yard, London for the preview of the new Park
+              Seo-Bo exhibition curated by Katharine Kostyál. Curated by
+              Katharine Kostyál
+            </span>
+          </div>
+        </div>
+      </article>
+      <article className="channel">
+        <div className="article-head">
+          <span className="font-face-bold title">Channel</span>
+          <span>
+            <Link className="banner-link" to="#">
+              View All
+            </Link>
+          </span>
+          <img src={arrow} alt="arrow" />
+        </div>
+        <a href="#">
+          <embed className="youtube" src={Youtube} alt="Video from Youtube" />
+        </a>
+        <div className="article-main">
+          <div>
+            <span className="font-face-bold title">
+              Anselm Kiefer in conversation with Tim Marlow in the Auditorium
+            </span>
+          </div>
+          <div>
+            <span style={{ fontSize: '19px' }}>
+              In this film Anselm Kiefer discusses his work and his exhibition
+              'Walhalla' at White Cube Bermondsey with Tim Marlow.
+            </span>
+          </div>
+        </div>
+      </article>
+      <article className="artists">
+        <div className="article-head">
+          <span className="font-face-bold title">Artists</span>
+        </div>
+        <div className="article-main artist-list">
+          <ul>
+            {data.artistSet1.map((x) => (
+              <li>
+                <Link
+                  className="artist-link"
+                  to="#"
+                  onMouseEnter={displayImage}
+                  onMouseLeave={hideImage}
+                >
+                  {x.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {data.artistSet2.map((x) => (
+              <li>
+                <Link
+                  className="artist-link"
+                  to="#"
+                  onMouseEnter={displayImage}
+                  onMouseLeave={hideImage}
+                >
+                  {x.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {data.artistSet3.map((x) => (
+              <li>
+                <Link
+                  className="artist-link"
+                  to="#"
+                  onMouseEnter={displayImage}
+                  onMouseLeave={hideImage}
+                >
+                  {x.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <img className="artist-image" src={Artist} alt="Artist_Image" />
+        </div>
+      </article>
+    </Container>
+  );
+}
